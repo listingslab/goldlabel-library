21c485bb2207312f5d8d289c341aabcfddf738af
Chris D <listingslab@gmail.com>
2024-06-22 16:40:26 +0100

    chicken

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://listingslab.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By listingslab
             {/* <Image
               src="/svg/vercel.svg"
               alt="Vercel Logo"
@@ -24,7 +24,6 @@ export default function Home() {
               height={24}
               priority
             /> */}
-            listingslab
           </a>
         </div>
       </div>
@@ -32,10 +31,10 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/svg/next.svg"
+          src="/svg/chicken.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={256}
+          height={256}
           priority
         />
       </div>
