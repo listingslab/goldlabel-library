b8510daa1684e73d62af600e08527bcb23898413
Chris D <listingslab@gmail.com>
2024-06-27 19:46:39 +0100

    Mobile Layout

--- GLLibrary/Layout/index.tsx
+++ GLLibrary/Layout/index.tsx
@@ -1,9 +1,11 @@
 import Layout from './Layout';
 import Desktop from './Desktop';
 import Mobile from './Mobile';
+import DeathScroll from "./DeathScroll";
 
 export {
   Layout,
   Desktop,
   Mobile,
+  DeathScroll,
 };
