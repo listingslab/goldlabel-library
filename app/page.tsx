a9537301346fcf889a7efd5d83ec4a43646e91c9
Chris D <listingslab@gmail.com>
2024-06-27 20:31:59 +0100

    Fixes lint

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         src="/webp/mcjob.webp"
       />
       <Typography variant="body2" gutterBottom>
-      In Douglas Coupland's novel *Generation X: Tales for an Accelerated Culture*, a "McJob" refers to a low-paying, low-prestige, dead-end job that requires few skills and offers little chance of advancement. The term is derived from jobs typically found at fast-food restaurants like McDonald's, symbolizing work that is perceived as meaningless and unsatisfying. McJobs are characterized by their monotonous nature, lack of benefits, and the feeling of being easily replaceable, reflecting the disillusionment and economic instability faced by Generation X.
+      In Douglas Coupland&apos;s novel *Generation X: Tales for an Accelerated Culture*, a McJob refers to a low-paying, low-prestige, dead-end job that requires few skills and offers little chance of advancement. The term is derived from jobs typically found at fast-food restaurants like McDonald&apos;s, symbolizing work that is perceived as meaningless and unsatisfying. McJobs are characterized by their monotonous nature, lack of benefits, and the feeling of being easily replaceable, reflecting the disillusionment and economic instability faced by Generation X.
       </Typography>
 
     </main>
