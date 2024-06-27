e0f77f2c1b26ebc0fcaf3f65f2f4d280bea6aa11
Chris D <c.dorward@vu.city>
2024-06-27 18:17:58 +0100

    Adds many fings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import {Library, PermaNav} from "../lib";
+import {Library} from "../lib";
 
 export const metadata: Metadata = {
   title: "Library",
