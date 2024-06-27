ea713491a36da7a1a35515186d8f39c36f78d3ba
Chris D <listingslab@gmail.com>
2024-06-27 21:01:13 +0100

    Enables testing with jest

---
+++ __tests__ /StartTests.test.tsx
@@ -0,0 +1,7 @@
+
+
+describe('Start Tests', () => {
+  it('The sun always rises', () => {
+    expect(true);
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
